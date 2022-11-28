859544e2b9ce153e34503cb8ec0bd2daa82e7adb
unknown <1847535232@qq.com>
2022-11-28 10:02:56 +0800

    first commit

---
+++ js/server.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(panel => {
+        panel.addEventListener('click', () => {
+            removeActiveClasses()
+            panel.classList.add('active');
+        })
+    })
+
+    function removeActiveClasses() {
+        panels.forEach(panel => {
+            panel.classList.remove('active')
+        })
+    }
+}
